fb96f6035449700f8cae37d71364b48d58d3fdd4
Ashwani Gupta <ashwanigupta125@gmail.com>
2018-02-10 23:11:48 +0530

    Add endpoints /vote-native, /noms-native and /users (#7)

--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -61,6 +61,66 @@ def get_events():
     return jsonify(data=dummy_data)
 
 
+@app.route('/noms-native')
+def noms_native():
+    dummy_data = [
+        {
+            "title": "Selfie Contest",
+            "subtitle": "Who's the best looking?",
+            "tags": "#faces #selfies",
+            "deadline": '5days',
+            "phase": 'running',
+            "nominationData": [
+            {
+                "Username": "ymmIADMSK",
+                "Filename": "QLhosFCXRngD",
+                "Description": "LuqGEzcASVDVcFIAGPWM",
+                "Submission": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Iron_Man_bleeding_edge.jpg/220px-Iron_Man_bleeding_edge.jpg"
+            },
+            {
+                "Username": "gYpfSMWpXFf",
+                "Filename": "KmgnPmWM",
+                "Description": "ycxbzbIFbRGsymaB",
+                "Submission": "https://vignette.wikia.nocookie.net/ironman/images/2/21/47.jpg"
+            },
+            {
+                "Username": "DQuClE",
+                "Filename": "SHBIgOwkvMfV",
+                "Description": "hBZuPcmsJUDOrXzTPowLY",
+                "Submission": "https://www.sideshowtoy.com/assets/products/400310-iron-man-mark-iii/lg/marvel-iron-man-mark-3-life-size-figure-400310-08.jpg"
+            },
+            {
+                "Username": "DZTFNUgiCb",
+                "Filename": "pAHlFdbb",
+                "Description": "MmGdoODojEVkypGLxmbwBo",
+                "Submission": "https://images-na.ssl-images-amazon.com/images/I/91qvAndeVYL._RI_.jpg"
+            },
+            {
+                "Username": "FHCegEPGd",
+                "Filename": "AeBNKdu",
+                "Description": "FcuntDPGFeKOtDWUVW",
+                "Submission": "https://vignette.wikia.nocookie.net/marveldatabase/images/0/06/Iron_Man_Armor_Model_37.jpg"
+            }
+        ]
+        }
+    ]
+    return jsonify(data=dummy_data)
+
+
+@app.route('/vote-native')
+def vote_native():
+    dummy_data = [
+        {
+            "title": "Audiophilia",
+            "subtitle": "Anyone's gonna rule the music league here?",
+            "tags": "#recordings #songs",
+            "deadline": '4days',
+            "phase": 'open'
+        }
+    ]
+    return jsonify(data=dummy_data)
+
+
 @app.route('/vote', methods=['POST'])
 def vote():
     '''Gives vote to specified event'''
@@ -93,3 +153,14 @@ def results():
         }
     ]
     return jsonify(data=resultsData)
+
+
+@app.route('/users')
+def users():
+    dummy_data = {
+        "Name": "Tony Stark",
+        "Avatar": "http://www.telegraph.co.uk/content/dam/films/2016/04/28/tonystark2-xlarge_trans_NvBQzQNjv4BqeuRHplZSizlnIpEPA_wgci-oMky92GODLj7YayikDrk.jpg",
+        "Score": 7,
+        "Latest": "Robot Hacking"
+    }
+    return jsonify(data=dummy_data)
