e882d4e89e4ff4eb6eb2d8e627f260136cc6a156
Ganesh Ramkumar <science.ganesh007@gmail.com>
2018-02-13 01:19:53 +0530

    Added React Native Code

---
+++ react-native-app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AsyncStorage } from 'react-native';
+
+import AuthNavigator from './navigators/AuthNavigator.js';
+import HomeNavigator from './navigators/HomeNavigator.js';
+
+var auth_token = AsyncStorage.getItem('HASURA_AUTH_TOKEN');
+bool = auth_token!=null;
+const toOpen =  true ? HomeNavigator : AuthNavigator;
+
+export default toOpen
