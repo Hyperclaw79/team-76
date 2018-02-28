742a5558ad849c001c072c0292d3520e840dceff
Ashwani Gupta <ashwanigupta125@gmail.com>
2018-02-28 19:21:54 +0530

    Update schema

--- microservices/api/src/models.py
+++ microservices/api/src/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
 
     hasura_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    avatar_file_id = db.Column(db.String)
+    avatar_file_link = db.Column(db.String)
     score = db.Column(db.Integer, server_default='0')
     last_event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
 
@@ -34,10 +34,10 @@ class Nomination(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
-    hasura_id = db.Column(db.String, db.ForeignKey('user.hasura_id'), nullable=False)
+    hasura_id = db.Column(db.Integer, db.ForeignKey('user.hasura_id'), nullable=False)
     filename = db.Column(db.String, nullable=False)
     desc = db.Column(db.String, server_default='No description')
-    file_id = db.Column(db.String, nullable=False)
+    file_link = db.Column(db.String, nullable=False)
 
 
 db.create_all()
